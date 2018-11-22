bacde707bc09eb25ee13b6444684de5f3a6d2ef2
MinkOrange <your_email@youremail.com>
2018-11-22 11:11:42 +0800

    add maven compiler plugin

--- src/test/java/com/smart/aspectj/basic/DeclaredParentsTest.java
+++ src/test/java/com/smart/aspectj/basic/DeclaredParentsTest.java
@@ -4,6 +4,7 @@ import com.smart.Seller;
 import com.smart.Waiter;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class DeclaredParentsTest {
@@ -14,7 +15,7 @@ public class DeclaredParentsTest {
         Waiter waiter = (Waiter)ctx.getBean("waiter");
         waiter.greetTo("John");
         waiter.serveTo("John");                     //添加serveTo()方法的测试
-        waiter.greetTo("Tom");                                                   //未写完测试code
+        waiter.greetTo("Tom");                                                   //未写完测试code   //现已写完
         Seller seller = (Seller)waiter;
         seller.sell("Beer","John");
     }
